7ff278c67041f4338b346e9b9047bdef2d3687aa
NeetYourCreator <rzshamir@icloud.com>
2023-10-22 10:28:07 -0400

    create event methods

--- src/components/SplitViewPanels.tsx
+++ src/components/SplitViewPanels.tsx
@@ -3,6 +3,7 @@ import { SplitViewPanelsProps } from './types';
 
 import './styles/split-view-panels.css';
 
+const minWidth = 75;
 const LeftPanel: React.FunctionComponent<{
   leftWidth: number | undefined;
   setLeftWidth: (value: number) => void;
@@ -29,6 +30,43 @@ export const SplitViewPanels: React.FunctionComponent<SplitViewPanelsProps> = ({
   className,
 }) => {
   const [leftWidth, setLeftWidth] = useState<undefined | number>(undefined);
+  const [separatorXPosition, setSeparatorXPosition] = useState<
+    undefined | number
+  >(undefined);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  const splitPanelRef = createRef<HTMLDivElement>();
+
+  const onMouseDown = (e: React.MouseEvent) => {
+    setSeparatorXPosition(e.clientX);
+    setIsDragging(true);
+  };
+
+  const onTouchStart = (e: React.TouchEvent) => {
+    setSeparatorXPosition(e.touches[0].clientX);
+    setIsDragging(true);
+  };
+
+  const onMove = (clientX: number) => {
+    if (isDragging && leftWidth && separatorXPosition) {
+      const newLeftWidth = leftWidth + clientX - separatorXPosition;
+      setSeparatorXPosition(clientX);
+
+      if (newLeftWidth < minWidth) {
+        setLeftWidth(minWidth);
+        return;
+      }
+
+      if (splitPanelRef.current) {
+        const splitPanelWidth = splitPanelRef.current.clientWidth;
+        if (newLeftWidth > splitPanelWidth - minWidth) {
+          setLeftWidth(splitPanelWidth - minWidth);
+          return;
+        }
+      }
+      setLeftWidth(newLeftWidth);
+    }
+  };
 
   return (
     <section className="splitView">
