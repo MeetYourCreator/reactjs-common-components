2b9a573a51dcddad1b55dc36aeae222ad0efd002
MeetYourCreator <rzshamir@icloud.com>
2023-10-26 16:13:17 -0400

    refactor Modal

--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -1,33 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ModalPresentational } from './ModalPresentational';
 
 export const ModalContainer: React.FunctionComponent = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const modalRef = useRef<HTMLDialogElement | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    setIsModalOpen(true);
-  }, [isModalOpen]);
-
-  useEffect(() => {
-    const modalElement = modalRef.current;
-    if (modalElement) {
-      if (isModalOpen) {
-        modalElement.showModal();
-      } else {
-        modalElement.close();
-      }
-    }
-  });
-
   return (
     <div>
-      <ModalPresentational modalRef={modalRef} onClose={handleCloseModal} />
+      <button onClick={() => setIsModalOpen(true)} />
+      {isModalOpen && <ModalPresentational onClose={handleCloseModal} />}
     </div>
   );
 };
