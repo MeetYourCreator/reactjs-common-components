89e8a9ec9707a6dd5569d9c5928a1ef1fa10c2ef
MeetYourCreator <rzshamir@icloud.com>
2023-10-27 11:58:54 -0400

    refactor Modal logic

--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -4,14 +4,10 @@ import { ModalPresentational } from './ModalPresentational';
 export const ModalContainer: React.FunctionComponent = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <div>
-      <button className='showModalBtn' onClick={() => setIsModalOpen(true)} />
-      {isModalOpen && <ModalPresentational onClose={handleCloseModal} />}
+      <button className="showModalBtn" onClick={() => setIsModalOpen(true)} />
+      {isModalOpen && <ModalPresentational setIsModalOpen={setIsModalOpen} />}
     </div>
   );
 };
