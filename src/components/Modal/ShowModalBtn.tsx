fea8ac9cf4f7a84498fff11775421cac6c997459
MeetYourCreator <rzshamir@icloud.com>
2023-10-28 17:16:16 -0400

    renasme all stylesheets to styles.css

--- src/components/Modal/ShowModalBtn.tsx
+++ src/components/Modal/ShowModalBtn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useBoolean } from '../useBoolean';
 import { Modal } from './Modal';
 import { ShowModalBtnProps } from './types';
+import './styles.css';
 
 export const ShowModalBtn: React.FunctionComponent<ShowModalBtnProps> = ({
   btnName,
