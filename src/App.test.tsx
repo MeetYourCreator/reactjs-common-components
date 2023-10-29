beb4dcb779db8a158f432c5e3957831b8312c00c
MeetYourCreator <rzshamir@icloud.com>
2023-10-28 20:33:58 -0400

    init test suites

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders SplitViewPanels component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divider = screen.getByText(/:::/i);
+  expect(divider).toBeInTheDocument();
 });
