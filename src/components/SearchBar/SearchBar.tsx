5fd711aaa10174940e5b464dca41a44378cf396a
MeetYourCreator <rzshamir@icloud.com>
2023-10-30 15:18:06 -0400

    add logic to SearchBar input element

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface formDataProps {
+  searchValue: string;
+}
 
 export const SearchBar = () => {
+  const [inputField, setInputField] = useState<string>('');
+  const [formData, setFormData] = useState<any>({
+    searchValue: '',
+  });
+  console.log('inputField: ', inputField);
+  console.log('formData.searchValue: ', formData, formData.searchValue);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setFormData({ searchValue: inputField });
+    setInputField('');
+  };
+
   return (
-    <input
-      className="searchBar"
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => e.target.value}
-    />
+    <form onSubmit={handleSubmit}>
+      <input
+        className="searchBar"
+        value={inputField}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInputField(e.target.value);
+        }}
+      />
+    </form>
   );
 };
