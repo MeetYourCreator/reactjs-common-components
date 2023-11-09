ad1ed4c4293b18bc8d85f6c4cb67acee531620ca
MeetYourCreator <rzshamir@icloud.com>
2023-11-09 11:34:48 -0500

    create Calendar component

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,41 +1,12 @@
-import { useState, useEffect } from 'react';
-import { Day } from './Day';
-import { Header } from './Header';
-import { DateObj } from './types';
-import { useDate } from '../../hooks/useDate';
-import './styles.css';
+import { useState } from 'react';
+import { Calendar } from '../index';
+
+// import './styles.css';
 
 export const DatePicker = () => {
-  const [nav, setNav] = useState<number>(0);
-  const { days, dateDisplay } = useDate(nav);
-
-  const weekdays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-
   return (
     <>
-      <Header
-        dateDisplay={dateDisplay}
-        onNext={() => setNav(nav + 1)}
-        onBack={() => setNav(nav - 1)}
-      />
-      <header className="calendarHeader">
-        {weekdays.map((weekday) => {
-          return <div>{weekday}</div>;
-        })}
-      </header>
-      <div className="calendarBody">
-        {days.map((date, index) => {
-          return <Day key={index} date={date} />;
-        })}
-      </div>
+      <Calendar />
     </>
   );
 };
