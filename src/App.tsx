8f7a1073e85632666d0a08c631550685216a16db
MeetYourCreator <rzshamir@icloud.com>
2023-11-11 13:18:14 -0500

    style app for more efficient presentation

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,22 @@ export const App = () => {
   return (
     <>
       <section className="appContainer">
-        <div className="screenContainer">
+        <div className="screenContainer panels">
           <SplitViewPanels
             leftPanel={<div>Left Panel Content</div>}
             rightPanel={<div>Right Panel Content</div>}
           />
         </div>
-        <div className="screenContainer">
-          <Modal btnName="Submit" />
-        </div>
-        <div className="screenContainer">
-          <SearchBar />
-        </div>
-        <div className="screenContainer">
+        <div className="screenContainer datepicker">
           <DatePicker />
         </div>
-        <div className="screenContainer">
+        <div className="screenContainer modal">
+          <Modal btnName="Submit" />
+        </div>
+        <div className="screenContainer searchBar">
+          <SearchBar />
+        </div>
+        <div className="screenContainer list">
           <List />
         </div>
       </section>
