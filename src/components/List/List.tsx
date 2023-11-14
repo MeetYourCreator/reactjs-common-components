f0eed03f2f5e57759174a8a6b5b823b118be11b1
MeetYourCreator <rzshamir@icloud.com>
2023-11-14 18:01:07 -0500

    working api algo

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -27,48 +27,48 @@ export const List = () => {
           case 'object':
             //if the object is not an array
             if (Array.isArray(entry) === false) {
-              console.log('object is not an array, object is:', entry);
-              return Object.entries(entry).map((obj: any) => {
-                console.log('object is :', obj[1], typeof obj[1]);
-
-                if (typeof obj[1] === 'string') {
-                  console.log('string obj:', obj[1]);
-                  return (
-                    <ul>
-                      <li>{obj[1]}</li>
-                    </ul>
-                  );
-                } else if (typeof obj[1] === 'string') {
-                  return (
-                    <ul>
-                      <li>{obj}</li>
-                    </ul>
-                  );
-                } else if (typeof obj[1] === 'boolean') {
-                  return (
-                    <ul>
-                      <li>{obj}</li>
-                    </ul>
-                  );
-                } else if (typeof obj[1] === 'object') {
-                  (function deepTraversal(obj: any, index) {
-                    console.log('deepTraversal object is:', obj);
-                    for (const key in obj) {
-                      // console.log('deepTraversal object is:', obj);
-                      if (typeof obj[key] === 'object') {
-                        deepTraversal(obj[key]);
-                      } else {
+              return (function deepTraversal() {
+                return Object.entries(entry).map((obj: any) => {
+                  //renders string values of obj
+                  if (typeof obj[1] === 'string') {
+                    return (
+                      <ul>
+                        <li>{obj[1]}</li>
+                      </ul>
+                    );
+                    //renders number values of obj
+                  } else if (typeof obj[1] === 'number') {
+                    return (
+                      <ul>
+                        <li>{obj[1]}</li>
+                      </ul>
+                    );
+                    //renders boolean values of obj
+                  } else if (typeof obj[1] === 'boolean') {
+                    return (
+                      <ul>
+                        <li>{obj[1]}</li>
+                      </ul>
+                    );
+                    //Goodup to this point 11/14
+                    //
+                    //
+                    //
+                    //what remains is if obj[1] ia an array or an object
+                    //if obj[1] is not an array
+                  } else if (Array.isArray(obj[1]) === false) {
+                    (function deepTraversal() {
+                      return Object.entries(entry).map((obj: any) => {
                         return (
                           <ul>
-                            {/* console.log('deepTraversal arrEntry:', obj[key]) */}
-                            <li key={`${index}${key}`}>{obj[key]}</li>
+                            <li>{obj[1]}</li>
                           </ul>
                         );
-                      }
-                    }
-                  })(obj);
-                }
-              });
+                      });
+                    })();
+                  }
+                });
+              })();
             } else {
               // (function deepTraversal(obj: any, index) {
               //   for (const key in obj) {
