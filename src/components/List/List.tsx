ff1411d2e7ce79471488cb9c5d99024ec7894d80
MeetYourCreator <rzshamir@icloud.com>
2023-11-15 16:39:19 -0500

    refactor to switchboard

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { data } from './utils';
 import './styles.css';
 
@@ -39,89 +40,37 @@ export const List = () => {
               </ul>
             );
           case 'object':
-            switch (Array.isArray(entry)) {
-              case false:
-                Object.entries(entry).map((obj: any) =>
-                  // console.log(
-                  //   'obj[1]:',
-                  //   obj[1],
-                  //   typeof obj[1],
-                  //   Array.isArray(obj[1])
-                  // );
-
-                  Array.isArray(obj[1])
-                    ? obj[1].map((el: any, index: number) => {
-                        console.log('el', Array.isArray(el));
-                        return (
-                          <ul>
-                            <li key={index}>{el}</li>
-                          </ul>
-                        );
-                      })
-                    : console.log('no')
-                );
-                break;
-              case true:
-            }
-            //if the object is not an array
-            if (Array.isArray(entry) === false) {
-              return (function deepTraversal(entry: any) {
-                return Object.entries(entry).map((obj: any) => {
-                  if (typeof obj[1] === 'object') {
-                    deepTraversal(obj[1]);
-                  } else {
-                    return (
-                      <ul>
-                        <li>{obj[1]}</li>
-                      </ul>
-                    );
-                  }
-                });
-              })(entry);
-            } else {
-              return entry.map((obj: any) => {
+            // switch (Array.isArray(entry)) {
+            //   case false:
+            return Object.entries(entry).map((obj: any, index: number) => {
+              if (typeof obj[1] !== 'object') {
                 return (
                   <ul>
-                    <li>{obj[1]}</li>
+                    <li key={`2-${index}`}>{obj[1]}</li>
                   </ul>
                 );
-              });
-            }
-          //Goodup to this point 11/14
-          //
-          //
-          //
-          //what remains is if obj[1] ia an array or an object
-          //if obj[1] is not an array
-          //       } else if (Array.isArray(obj[1]) === false) {
-          //         (function deepTraversal() {
-          //           return Object.entries(entry).map((obj: any) => {
-          //             return (
-          //               <ul>
-          //                 <li>{obj[1]}</li>
-          //               </ul>
-          //             );
-          //           });
-          //         })();
-          //       }
-          //     });
-          //   })();
-          // } else {
-          //   // (function deepTraversal(obj: any, index) {
-          //   //   for (const key in obj) {
-          //   //     if (typeof obj[key] === 'object') {
-          //   //       deepTraversal(obj[key]);
-          //   //     } else {
-          //   //       return (
-          //   //         <ul>
-          //   //           console.log('deepTraversal entry:', obj[key])
-          //   //           <li key={`${index}${key}`}>{obj[key]}</li>
-          //   //         </ul>
-          //   //       );
-          //   //     }
-          //   //   }
-          //   // })(entry);
-          // }
+              } else if (Array.isArray(obj[1])) {
+                console.log(entry, obj[1]);
+
+                return obj[1].map((element: any, index: number) => {
+                  console.log('element', element);
+                  return (
+                    <ul>
+                      <li key={`3-${index}`}>{element[1]}</li>
+                    </ul>
+                  );
+                });
+              }
+              //if Array is not Array obj[1]
+              else if (!Array.isArray(obj[1])) {
+                return (
+                  <h1>
+                    obj[1] is not an array but an object. typeof obj[1]:{' '}
+                    {typeof obj[1]}
+                  </h1>
+                );
+              }
+            });
         }
       })}
     </div>
--- src/components/List/utils.ts
+++ src/components/List/utils.ts
@@ -8,14 +8,6 @@ export const data: Array<any> = [
     type: 'donut',
     name: 'Cake',
     ppu: 0.55,
-    batters: {
-      batter: [
-        { id: '3rd level 01', type: 'Regular' },
-        { id: '3rd level 02', type: 'Chocolate' },
-        { id: '3rd level 03', type: 'Blueberry' },
-        { id: '3rd level 04 ', type: "Devil's Food" },
-      ],
-    },
     topping: [
       { id: '2nd level 01', type: 'None' },
       { id: '2nd level 02', type: 'Glazed' },
@@ -25,15 +17,20 @@ export const data: Array<any> = [
       { id: '5003', type: 'Chocolate' },
       { id: '5004', type: 'Maple' },
     ],
+    batters: {
+      batter: [
+        { id: '3rd level 01', type: 'Regular' },
+        { id: '3rd level 02', type: 'Chocolate' },
+        { id: '3rd level 03', type: 'Blueberry' },
+        { id: '3rd level 04 ', type: "Devil's Food" },
+      ],
+    },
   },
   {
     id: '1st level 02',
     type: 'donut',
     name: 'Raised',
     ppu: 0.55,
-    batters: {
-      batter: [{ id: '1001', type: 'Regular' }],
-    },
     topping: [
       { id: '5001', type: 'None' },
       { id: '5002', type: 'Glazed' },
@@ -41,23 +38,26 @@ export const data: Array<any> = [
       { id: '5003', type: 'Chocolate' },
       { id: '5004', type: 'Maple' },
     ],
+    batters: {
+      batter: [{ id: '1001', type: 'Regular' }],
+    },
   },
   {
     id: '1st level 03',
     type: 'donut',
     name: 'Old Fashioned',
     ppu: 0.55,
-    batters: {
-      batter: [
-        { id: '1001', type: 'Regular' },
-        { id: '1002', type: 'Chocolate' },
-      ],
-    },
     topping: [
       { id: '5001', type: 'None' },
       { id: '5002', type: 'Glazed' },
       { id: '5003', type: 'Chocolate' },
       { id: '5004', type: 'Maple' },
     ],
+    batters: {
+      batter: [
+        { id: '1001', type: 'Regular' },
+        { id: '1002', type: 'Chocolate' },
+      ],
+    },
   },
 ];
