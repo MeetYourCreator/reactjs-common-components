547469fffd1eb77fa8408cda5f8c5b279b2e7383
MeetYourCreator <rzshamir@icloud.com>
2023-11-24 11:19:51 -0500

    fix styling for Slider

--- src/components/Slider/SliderContainer.tsx
+++ src/components/Slider/SliderContainer.tsx
@@ -3,14 +3,7 @@ import { SliderProps } from './types';
 
 export const SliderContainer = ({ urlArray }: SliderProps) => {
   return (
-    <div
-      style={{
-        maxWidth: '1200px',
-        width: '100%',
-        height: '100%',
-        margin: '0 auto',
-      }}
-    >
+    <div className="sliderContainer">
       <Slider urlArray={urlArray} />
     </div>
   );
