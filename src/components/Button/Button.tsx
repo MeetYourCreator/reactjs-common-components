8b51c63066374ef8b71b2c0cc8cea4aaca79603d
MeetYourCreator <rzshamir@icloud.com>
2023-11-25 17:55:54 -0500

    add onHover and onHoverLeave to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,23 @@ import './styles.css';
 
 export const Button = ({ onClick, name, icon, styles }: ButtonProps) => {
   const buttonId = useId();
+
+  const onHover = (e: any) => {
+    e.target.style.background = 'black';
+  };
+
+  const onHoverLeave = (e: any) => {
+    e.target.style.background = 'transparent';
+  };
   return (
-    <button id={buttonId} className="button" onClick={onClick} style={styles}>
+    <button
+      id={buttonId}
+      className="button"
+      onClick={onClick}
+      onMouseEnter={onHover}
+      onMouseLeave={onHoverLeave}
+      style={styles}
+    >
       {name}
       {icon}
     </button>
