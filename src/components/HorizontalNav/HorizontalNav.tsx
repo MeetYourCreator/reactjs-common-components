0fd0e43d3622dedebb82b76116fdcd7c4373b13d
MeetYourCreator <rzshamir@icloud.com>
2023-12-24 10:37:30 -0500

    add threshold and rootMargin options for IntersectionObserver

--- src/components/HorizontalNav/HorizontalNav.tsx
+++ src/components/HorizontalNav/HorizontalNav.tsx
@@ -9,16 +9,17 @@ export const HorizontalNav = () => {
 
   //create Intersection Observer
   useEffect(() => {
-    const observer =
-      // document.addEventListener('DOMContentLoaded', () => {
-      new IntersectionObserver(
-        ([entry]) => {
-          console.log('entries', entry);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        console.log('entries', entry);
 
-          setIsIntersecting(entry.isIntersecting);
-        },
-        { rootMargin: '-300px' }
-      );
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        // rootMargin: '-300px',
+        threshold: 0.5,
+      }
+    );
     // });
     console.log('isIntersecting', isIntersecting);
     observer.observe(ref.current);
@@ -28,29 +29,21 @@ export const HorizontalNav = () => {
   useEffect(() => {
     if (isIntersecting) {
       ref.current
-        .querySelectorAll('div')
+        .querySelectorAll('.entry')
         .forEach((el: any) => el.classList.add('slide-in'));
     } else {
-      ref.current
-        .querySelectorAll('div')
-        .forEach((el: any) => el.classList.remove('slide-in'));
+      ref.current.querySelectorAll('article .entry').forEach((el: any) => {
+        el.classList.remove('slide-in');
+        // el.classList.add('slide-out');
+      });
     }
   }, [isIntersecting]);
 
   return (
-    <article
-      ref={ref}
-
-      // className="horizontalNavContainer"
-    >
+    <article ref={ref} className="horizontalNavContainer">
       <div className="entry">
         <Card styles={{ border: '1px solid black' }} />
       </div>
-      <div className="entry">
-        <Card styles={{ border: '1px solid black' }} />
-      </div>
-
-      <div className="entry">This is an entry</div>
 
       <div className="entry">This is the last entry</div>
     </article>
